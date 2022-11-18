e62833ce02548da4f3f7aa6c8340eceb97380d2e
remyeru <genovalemuelc@gmail.com>
2022-11-18 12:10:58 +0800

    g

--- Free.js
+++ Free.js
@@ -1,9 +1,15 @@
 var navSlide = ()=> {
     const bar = document.querySelector('.bar');
     const nav = document.querySelector('.nav-links');
+    const navLinks = nav.querySelectorAll('.nav-links li');
+        // toggle nav
 
     bar.addEventListener('click', ()=> {
-         nav.classList .toggle('.nav-active');   
+         nav.classList.toggle('.nav-active');   
+    });
+    // animate links
+    navLinks.forEach((link, Freelance)=>{
+        console.log(Freelance)
     });
 }
 navSlide();
