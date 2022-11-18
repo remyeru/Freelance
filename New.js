ab29e3a710c17c1d0a36c2563a9913d393e76177
remyeru <genovalemuelc@gmail.com>
2022-11-18 18:39:10 +0800

    d

--- New.js
+++ New.js
@@ -1,93 +1,75 @@
-goTO = (keyword) => {
-    const identifier = '0' + keyword;
-    const protfolio = document.querySelector(indetifier);
-    protfolio.scrollIntoView({behavior: 'smooth'});
-}
 window.addEventListener('DOMContentLoaded', event => {
-// nav bar function//
-var navbarShrink = function () {
-    const navbarCollapsible = document.body.querySelector('#mainNav');
-    if (!navbarCollapsible) {
-        return;
-    }
-    if (window.scrollY === 0) {
-        navbarCollapsible.classList.remove('navbar-shrink')
-    } else {
-        navbarCollapsible.classList.add('navbar-shrink')
-    }
 
-};
-// nav shrinker
-navbarShrink();
-// 
-document.addEventListener('scroll', navbarShrink);
-// 
-const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new navigation.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
-    // 
-const navbarToggler = document.body.querySelector('.navbar-toggler');
-const responsiveNavItems = [].slice.call(
-    document.querySelectorAll('#navbarResponsive .nav-link')
-);
-responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener('click', () => {
-        if (window.getComputedStyle(navbarToggler).display !== 'none') {
-            navbarToggler.click();
-            }
-         });
+    const sidebarWrapper = document.getElementById('sidebar-wrapper');
+    let scrollToTopVisible = false;
+    // Closes the sidebar menu
+    const menuToggle = document.body.querySelector('.menu-toggle');
+    menuToggle.addEventListener('click', event => {
+        event.preventDefault();
+        sidebarWrapper.classList.toggle('active');
+        _toggleMenuIcon();
+        menuToggle.classList.toggle('active');
+    })
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
+    scrollTriggerList.map(scrollTrigger => {
+        scrollTrigger.addEventListener('click', () => {
+            sidebarWrapper.classList.remove('active');
+            menuToggle.classList.remove('active');
+            _toggleMenuIcon();
+        })
     });
-});
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+
+    function _toggleMenuIcon() {
+        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
+        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-xmark');
+        if (menuToggleBars) {
+            menuToggleBars.classList.remove('fa-bars');
+            menuToggleBars.classList.add('fa-xmark');
+        }
+        if (menuToggleTimes) {
+            menuToggleTimes.classList.remove('fa-xmark');
+            menuToggleTimes.classList.add('fa-bars');
+        }
     }
-}
-// $(function(){
 
-// $('#sinopsi').click(function(){  // Activates the popups
-//   $('#popup1').fadeIn('fast')
-// });
+    // Scroll to top button appear
+    document.addEventListener('scroll', () => {
+        const scrollToTop = document.body.querySelector('.scroll-to-top');
+        if (document.documentElement.scrollTop > 100) {
+            if (!scrollToTopVisible) {
+                fadeIn(scrollToTop);
+                scrollToTopVisible = true;
+            }
+        } else {
+            if (scrollToTopVisible) {
+                fadeOut(scrollToTop);
+                scrollToTopVisible = false;
+            }
+        }
+    })
+})
 
-// $('#fitxa').click(function(){
-//   $('#popup2').fadeIn('fast')
-// });
+function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+};
 
-// $('#sinopsi').click(function(){
-//   $('#popup3').fadeIn('fast')
-// });
-
-/* //Note that if your HTML is properly nested you could easily have this command execute your popups instead of the three above
-
-$('.openlink').click(function(){
-  $(this)
-    .find('.popup')  // finds your nested popup div
-    .fadeIn('fast')
-  ;
-});
-
-*/
-
-// $('.closex').click(function(){  // closes the popup, when X is clicked
-//   $('.popup').fadeOut('fast');
-// });
-
-// $('.popup').keypress(function(e){  // close popup via ESC key.
-//   if(e.keyCode==27){
-//     $(this).fadeOut('fast');
-//   }
-// }); 
-
-// $('.popup').css({opacity: "0.7"}); // copies over the transparency 
-
-// });
-$('.first-popup-link, .second-popup-link, .third-popup-link').magnificPopup({
-    closeBtnInside:true
-});
+function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+};
